67f4b29786319e96cfba3d61a48c50385f198764
only-einsteined <enhoinehd@yeah.net>
2021-02-05 13:37:03 +0800

    podspec

--- Example/Pods/Target Support Files/OldedDemoViewModel/OldedDemoViewModel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OldedDemoViewModel : NSObject
-@end
-@implementation PodsDummy_OldedDemoViewModel
-@end
--- Example/Pods/Target Support Files/Pods-OldedDemoViewModel_Tests/Pods-OldedDemoViewModel_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OldedDemoViewModel_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_OldedDemoViewModel_Tests
-@end
